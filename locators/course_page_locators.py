e429fb8f6518c1eb418fb68acdd43c07d190da0e
kseniaglivko <73233220+kseniaglivko@users.noreply.github.com>
2021-09-13 20:07:53 +0300

    Deleted create_course_link locator.

--- locators/course_page_locators.py
+++ locators/course_page_locators.py
@@ -3,11 +3,6 @@ from selenium.webdriver.common.by import By
 
 class CoursePageLocators:
     COURSES_HEADER = (By.XPATH, "//a[@href='#linkcourses']")
-    CREATE_COURSE_LINK = (
-        By.XPATH,
-        "//a[@href='https://qacoursemoodle.innopolis.university/course/edit.php"
-        "?category=0']",
-    )
     MANAGE_COURSES_BUTTON = (By.XPATH, "//button[text()='Управление курсами']")
     DELETE_COURSE_BUTTON = (By.CLASS_NAME, "action-delete")
     CONFIRM_DELETE_BUTTON = (By.XPATH, "//button[text()='Удалить']")
